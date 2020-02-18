fd1469581e4f4513235b28125036f1f59ccfecad
chandanrajhs <55313667+chandanrajhs@users.noreply.github.com>
2020-02-18 23:34:08 +0530

    Add files via upload

---
+++ set-1/PROGRAM4.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,n;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	for(j=0;j<(n-i);j++)
+	{
+		printf(" ");
+
+	}
+
+	for(k=0;k<i;k++)
+	{
+
+		if(k==0||k==i-1)
+		{
+		printf("*");
+		printf(" ");
+		}
+		else
+		printf("  ");
+	}
+	printf("\n");
+	if(i==n-1)
+	for(k=0;k<n;k++)
+	printf("* ");
+}
+getch();
+
+}
