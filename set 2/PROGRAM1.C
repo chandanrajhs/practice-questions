7572656a6c72d77062cb4b5f1bd27fac19e8eec3
chandanrajhs <55313667+chandanrajhs@users.noreply.github.com>
2020-02-19 23:41:50 +0530

    Update PROGRAM1.C

--- set 2/PROGRAM1.C
+++ set 2/PROGRAM1.C
@@ -1,19 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int n,i,j,z;
+int n,i,j;
 clrscr();
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
-z=1;
-for(j=0;j<n;j++)
+for(j=0;j<=i;j++)
 {
-	if(j<=i)
-	{
-	printf("%d",z);
-	z++;
-	}
+	printf("%d",j+1);
 }
 printf("\n");
 }
