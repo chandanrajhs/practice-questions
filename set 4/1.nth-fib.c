e06e6c346d7ad2c24d561a2507d19fc849d2d0d5
chandanrajhs <55313667+chandanrajhs@users.noreply.github.com>
2020-02-24 20:32:48 +0530

    Update 1.nth-fib.c

--- set 4/1.nth-fib.c
+++ set 4/1.nth-fib.c
@@ -1,15 +1,19 @@
-#include<stdio.h> 
-int fib(int n) 
-{ 
-   if (n <= 1) 
-      return n; 
-   return fib(n-1) + fib(n-2); 
-} 
-  
-int main () 
-{ 
+#include<stdio.h>
+int fib(int n)
+{
+   if(n == 1)      //Base condition
+        return 0;
+    else if(n == 2) //Base condition
+        return 1;
+    else
+        return fib(n-1) + fib(n-2);
+
+}
+
+int main ()
+{
   int n;
    scanf("%d",&n);
-  printf("%d", fib(n)); 
-  return 0; 
-} 
+  printf("%d", fib(n));
+  return 0;
+}
