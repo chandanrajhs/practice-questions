46f11a38542bfc45b8b19b302270651a60b1bdf4
chandanrajhs <55313667+chandanrajhs@users.noreply.github.com>
2020-03-19 00:31:26 +0530

    Create 3.sumPerfect.c

---
+++ set 7/3.sumPerfect.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+
+int isPerfectSquare(int number)
+{
+int i;
+float f;
+
+f=sqrt(number);
+i=f;
+
+if(i==f)
+return number;
+else
+return 0;
+}
+
+int main()
+{
+int n;
+scanf("%d",&n);
+int arr[n];
+int i;
+for(i = 0; i < n; i++)
+{
+scanf("%d",&arr[i]);
+}
+int sum = 0;
+for(i = 0; i < n; i++)
+{
+sum = sum + isPerfectSquare(arr[i]);
+}
+printf("%d",sum);
+return 0;
+}
