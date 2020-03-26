8c41dc2755d2818521e369408f036d451bcbb56d
chandanrajhs <55313667+chandanrajhs@users.noreply.github.com>
2020-03-26 14:57:37 +0530

    Update 4.eleSearch.c

--- set 9/4.eleSearch.c
+++ set 9/4.eleSearch.c
@@ -23,3 +23,72 @@ if(flag==0)
     printf("Element not found");
 return 0;
 }
+
+output:-
+
+ Enter size of the  array : 5
+Enter elements in array : 1 2 3 4 5
+Enter the element to be search 3
+
+The  element is at index of 2
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
+ //solutions in java
+public class searchElementinArray {
+    public static void main(String[] args) {
+
+
+    int[] arr={1,2,3,4};
+    int element=3;
+    int k=0;
+    for(int i=0;i<arr.length;i++)
+    {
+        if(arr[i]==element)
+        {
+            k=i;
+            break;
+
+        }
+
+    }
+        System.out.println("The element is at index of "+k);
+    }
+}
+output:-
+The element is at index of 2
